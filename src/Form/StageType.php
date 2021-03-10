7cc21dd4343d22ffc7fa8c9af526aa2a5f76675b
Gabin Raapoto <gabin.rpt@gmail.com>
2021-03-10 16:03:32 +0100

    save disque C

--- src/Form/StageType.php
+++ src/Form/StageType.php
@@ -3,11 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Stage;
+use App\Entity\Formation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class StageType extends AbstractType
 {
@@ -18,7 +20,12 @@ class StageType extends AbstractType
             ->add('mission',TextareaType::class)
             ->add('mail',EmailType::class)
             //->add('entreprise')
-            //->add('formations')
+            ->add('formations', EntityType::class, [
+                'class' => Formation::class,
+                'choice_label' => 'nom',
+                'multiple' => true,
+                'expanded' => true,
+            ]);
         ;
     }
 
